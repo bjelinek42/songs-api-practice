bcc8d15f4062ee80ea77f2e7d4f52489b4d73122
ben <bjelinek42@gmail.com>
2022-03-14 11:20:29 -0600

    index and show controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,13 @@
 class SongsController < ApplicationController
+
+  def index
+    songs = Song.all
+    render json: songs.to_json
+  end
+
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: song
+  end
+  
 end
