7d3c7a3aaeda7ed9aa1fa3ac9fb8f804605970c3
ben <bjelinek42@gmail.com>
2022-03-14 11:38:44 -0600

    patch/post/delete controllers

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,5 +9,23 @@ class SongsController < ApplicationController
     song = Song.find_by(id: params[:id])
     render json: song
   end
-  
+
+  def create
+    song = Song.new({title: params[:title], album: params[:album], artist: params[:artist], year: params[:year]})
+    song.save
+  end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.title = params[:title]
+    song.album = params[:album]
+    song.artist = params[:artist]
+    song.year = params[:year]
+    song.save
+  end
+
+  def destroy
+    song = Song.find_by(id: params[:id])
+    song.delete
+  end
 end
